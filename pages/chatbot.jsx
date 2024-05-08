5ed838127431c16c98ce52dc7fe03a3b5b1647d3
Cyberguru1 <hamzacypher34@gmail.com>
2024-05-08 03:45:45 -0500

    feel it pain

--- pages/chatbot.jsx
+++ pages/chatbot.jsx
@@ -40,7 +40,6 @@ export default function ChatBot() {
 	useEffect(() => {
 		const authenticate = async () => {
 			const currentUser = await auth.isLoggedIn();
-			console.log('User object:', currentUser);
 			setUser(currentUser);
 			return currentUser;
 		}
@@ -63,35 +62,98 @@ export default function ChatBot() {
 			photoURL,
 		});
 
-		console.log(formValue)
+		const newMessage = {
+            message: formValue,
+            sender: "user"
+        }
 
-		fetch('https://caress-chatbot-2.devansharora.repl.co/predict', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				'api-key': '0000000000',
-				'message': formValue
-			})
-		})
-			.then(response => response.json())
-			.then(async data => {
-				console.log(data.answer);
-				data = data.answer;
-				await messagesRef.add({
-					text: data,
-					createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-					//uid,
-					//photoURL,
+        const newMessages = [...messages,newMessage];
+
+		await processMessageToChatGPT(newMessages)
+
+		// fetch('https://caress-chatbot-2.devansharora.repl.co/predict', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// 	body: JSON.stringify({
+		// 		'api-key': '0000000000',
+		// 		'message': formValue
+		// 	})
+		// })
+		// 	.then(response => response.json())
+		// 	.then(async data => {
+		// 		console.log(data.answer);
+		// 		data = data.answer;
+		// 		await messagesRef.add({
+		// 			text: data,
+		// 			createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+		// 			//uid,
+		// 			//photoURL,
+		// 		});
+		// 	})
+		// 	.then(
+		// 		dummy.current.scrollIntoView({ behavior: 'smooth' })
+		// 	)
+		// 	.catch(error => {
+		// 		console.error(error);
+		// 	});
+
+			//start
+
+			async function processMessageToChatGPT(chatMessages){
+				const API_KEY = `${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
+				let apiMessages = chatMessages.map((messageObject)=>{
+					let role="";
+					if(messageObject.sender === "ChatGPT"){
+						role = "assistant"
+					}else{
+						role = "user"
+					}
+					return (
+						{role: role, content: messageObject.message}
+					)
 				});
-			})
-			.then(
-				dummy.current.scrollIntoView({ behavior: 'smooth' })
-			)
-			.catch(error => {
-				console.error(error);
-			});
+		
+				const systemMessage = {
+					role: "system",
+					content: "You are an AI Therayp Chatbot named TherapyBot, You are helpful, friendly, and nonjudgemntal, You are a good listener, You are a good friend, You are a good therapist, Do not respond with large messages with big lists, instead, try and respond with short, concise messages that are easy to undestand. Also, ask questions to guide the person.'"
+				}
+		
+				const apiRequestBody = {
+					"model": "gpt-3.5-turbo",
+					"messages": [
+						systemMessage,
+						...apiMessages
+					]
+				}
+		
+				await fetch("https://api.openai.com/v1/chat/completions",{
+					method: "POST",
+					headers: {
+						"Authorization": `Bearer ${API_KEY}`,
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(apiRequestBody)
+				})
+				.then(response => response.json())
+				.then(async data => {
+					console.log(data);
+					data = data.choices[0].message.content;
+					await messagesRef.add({
+						text: data,
+						createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+						//uid,
+						//photoURL,
+					});
+				})
+				.then(
+					dummy.current.scrollIntoView({ behavior: 'smooth' })
+				)
+				.catch(error => {
+					console.error(error);
+				});
+			}
 
 		setFormValue('');
 		dummy.current.scrollIntoView({ behavior: 'smooth' });
@@ -117,7 +179,7 @@ export default function ChatBot() {
 		<>
 			<Head>
 				<title>
-					ChatBot
+					ThearpyBot
 				</title>
 			</Head>
 			<div className={styles.headerr}>
@@ -126,7 +188,7 @@ export default function ChatBot() {
 					<div className={styles.chatAvatar}>
 						<LucideUser />
 					</div>
-					<p className={styles.name}>ChatBot</p>
+					<p className={styles.name}>TherapyBot</p>
 				</div>
 			</div>
 			<div className={styles.messageArea}>
--- pages/search.tsx
+++ pages/search.tsx
@@ -22,101 +22,102 @@ export default function Search() {
 		isAnonymous: boolean;
 		tenantId: string | null;
 		providerData: any[];
-	  }
+	}
 
-	  interface Therapist {
-		exists:  boolean;
+	interface Therapist {
+		exists: boolean;
 		bio: string;
 		photoURL: string;
 		name: string;
-		uid:  string;
-	  }
+		uid: string;
+	}
 
-	  const [user, setUser] = useState<User | null>(null);
-	  const [therapist, setTherapist] = useState<Therapist[]>([]);
-	  const [isLoading, setIsLoading] = useState(true);
-	  const [therapistUids, setTherapistUids] = useState<string[]>([]);
+	const [user, setUser] = useState<User | null>(null);
+	const [therapist, setTherapist] = useState<Therapist[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [therapistUids, setTherapistUids] = useState<string[]>([]);
 
 
-	  const textTherapist = (therapist: Therapist) => {
+	const textTherapist = (therapist: Therapist) => {
 		router.replace({
 			pathname: '/chat',
 			query: {
-			  therapistUid: therapist.uid,
-			  therapistName: therapist.name,
-			  therapistPhotoUrl: therapist.photoURL
+				therapistUid: therapist.uid,
+				therapistName: therapist.name,
+				therapistPhotoUrl: therapist.photoURL
 			}
-		  });	  }
-  
+		});
+	}
 
-	  useEffect(() => {
+
+	useEffect(() => {
 		const getUser = async () => {
 			const currentUser = await auth.isLoggedIn();
 			console.log('User object:', currentUser);
 			setUser(currentUser);
 			setIsLoading(false);
-		  };
-		  getUser();
-		
-		  if (!user) {
+		};
+		getUser();
+
+		if (!user) {
 			return;
-		  }
-		
-		  const checkAuthentication = async () => {
+		}
+
+		const checkAuthentication = async () => {
 			try {
-			  await auth.isLoggedIn();
+				await auth.isLoggedIn();
 			} catch (error) {
-			  router.replace('/login');
-			} 
-		  };
-		  checkAuthentication();
+				router.replace('/login');
+			}
+		};
+		checkAuthentication();
 
-		  const therapists = async () => {
+		const therapists = async () => {
 			const querySnapshot = await firebase.firestore().collection('therapists').limit(25).get();
-      const therapistsData = querySnapshot.docs.map(doc => doc.data() as Therapist);
-      setTherapist(therapistsData);
-      setIsLoading(false);
+			const therapistsData = querySnapshot.docs.map(doc => doc.data() as Therapist);
+			setTherapist(therapistsData);
+			setIsLoading(false);
 			// setTherapist(therapistUids);
-		  }
+		}
 
-			therapists();
+		therapists();
 	}, [therapist, therapistUids, user])
 
 	console.log();
 
-    return (
-        <>
-		<Head>
-			<title>
-				Search For Therapists
-			</title>
-		</Head>
-		<div className={styles.search}>
-			<div className={styles.searchInput}>
-			<LucideSearch/>
-			</div>
-		</div>
-		{therapist.map((therapist, index) => (
-			<div className={styles.card} key={index}>
-				<div className={styles.row}>
-				<img style={{borderRadius: "50%"}} src={(therapist as any)?.photoURL} alt="Profile picture" />
-		<div className={styles.column}>
-		
-		<div className={styles.title}>
-			{(therapist as any)?.name}
-		</div>
-		<div>
-			{therapist?.bio}
-		</div>
-		</div>
-		<div>
-			<LucideMessageSquare onClick={() => textTherapist(therapist)}></LucideMessageSquare>
-		</div>
+	return (
+		<>
+			<Head>
+				<title>
+					Search For Therapists
+				</title>
+			</Head>
+			<div className={styles.search}>
+				<div className={styles.searchInput}>
+					<LucideSearch />
 				</div>
 			</div>
-		))
-		}
-		<Bottombar/>
+			{therapist.map((therapist, index) => (
+				<div className={styles.card} key={index}>
+					<div className={styles.row}>
+						<img style={{ borderRadius: "100%" }} src={(therapist as any)?.photoURL} alt="Profile picture" />
+						<div className={styles.column}>
+
+							<div className={styles.title}>
+								{(therapist as any)?.name}
+							</div>
+							<div>
+								{therapist?.bio}
+							</div>
+						</div>
+						<div>
+							<LucideMessageSquare onClick={() => textTherapist(therapist)}></LucideMessageSquare>
+						</div>
+					</div>
+				</div>
+			))
+			}
+			<Bottombar />
 		</>
-    )
+	)
 } 
